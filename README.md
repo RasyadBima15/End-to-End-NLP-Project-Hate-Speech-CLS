60af3626b709db5513ad1b9056419668b0e736b8
RasyadBima15 <rasyadbimasatya04@gmail.com>
2025-07-21 13:26:41 +0800

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # End-to-End-NLP-Project-Hate-Speech-CLS
+
+## How to run?
+
+```bash
+conda create -n hate python=3.8 -y
+```
+
+```bash
+conda activate hate
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Rasyad",
+    author_email="rasyadbimasatya04@gmail.com",
+    packages=find_packages(),
+    install_requires=[],      
+)
